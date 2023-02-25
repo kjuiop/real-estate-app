387e2fced0e82632052d8f6498d16c50cd215c8f
kjuiop <copying_y@naver.com>
2023-02-25 13:43:52 +0900

    [#1] chore: admin application init gradle, yml

---
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package io.gig.realestate.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/25
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
