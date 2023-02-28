34d3335cece769570c2077166552121d01505c5e
kjuiop <copying_y@naver.com>
2023-03-01 00:17:33 +0900

    [#3] feat: security config apply

---
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/config/AdminConfiguration.java
@@ -0,0 +1,25 @@
+package io.gig.realestate.admin.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : JAKE
+ * @date : 2023/02/28
+ */
+@Slf4j
+@Configuration
+public class AdminConfiguration implements ApplicationContextAware, WebMvcConfigurer {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+}
