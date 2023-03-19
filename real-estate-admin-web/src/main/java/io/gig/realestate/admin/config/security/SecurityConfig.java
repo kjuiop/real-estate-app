68b1f318b9dceb2d9b01515d30ecbadb6f605b81
kjuiop <copying_y@naver.com>
2023-03-19 11:24:10 +0900

    [#9] fix: warn ignoring antMatchers Error fix

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/config/security/SecurityConfig.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/config/security/SecurityConfig.java
@@ -2,13 +2,12 @@ package io.gig.realestate.admin.config.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,11 +26,6 @@ public class SecurityConfig {
     private final AuthenticationFailureHandlerImpl authenticationFailureHandler;
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers("resources/**", "/node_modules/**" ,"/custom-modules/**", "/js/**", "/css/**", "/images/**", "/fonts/**", "/favicon.ico", "/error");
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
@@ -39,6 +33,19 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    @Order(0)
+    public SecurityFilterChain resources(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers((matchers) -> matchers.antMatchers("resources/**", "/node_modules/**" ,"/custom-modules/**", "/js/**", "/css/**", "/images/**", "/fonts/**", "/favicon.ico", "/error"))
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
+                .requestCache().disable()
+                .securityContext().disable()
+                .sessionManagement().disable();
+
+        return http.build();
     }
 
     @Bean
