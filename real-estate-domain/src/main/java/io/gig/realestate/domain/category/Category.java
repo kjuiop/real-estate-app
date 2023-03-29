fa92e2c787437d94ba0a8d820f4919167a303860
kjuiop <copying_y@naver.com>
2023-03-29 16:54:56 +0900

    [#19] feat: 카테고리

---
+++ real-estate-domain/src/main/java/io/gig/realestate/domain/category/Category.java
@@ -0,0 +1,52 @@
+package io.gig.realestate.domain.category;
+
+import io.gig.realestate.domain.common.YnType;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : JAKE
+ * @date : 2023/03/29
+ */
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Builder.Default
+    @Setter(AccessLevel.PRIVATE)
+    private int level = 1;
+
+    private String colorCode;
+
+    @Builder.Default
+    private int sortOrder = 0;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(length = 2)
+    private YnType activeYn = YnType.N;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Category> child = new ArrayList<>();
+
+}
