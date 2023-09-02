ce1e543fed1f72b0623ef2df66637b20bf9bf635
kjuiop <copying_y@naver.com>
2023-09-02 09:56:26 +0900

    [#28] feat: map sample page add

---
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/map/MapController.java
@@ -0,0 +1,28 @@
+package io.gig.realestate.admin.controller.map;
+
+import io.gig.realestate.domain.realestate.RealEstateSearchDto;
+import io.gig.realestate.domain.realestate.dto.RealEstateDetailDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : JAKE
+ * @date : 2023/04/14
+ */
+@Controller
+@RequestMapping("map")
+@RequiredArgsConstructor
+public class MapController {
+
+    @GetMapping("sample")
+    public String sample(Model model) {
+
+        RealEstateDetailDto dto = RealEstateDetailDto.emptyDto();
+        model.addAttribute("dto", dto);
+
+        return "map/map";
+    }
+}
