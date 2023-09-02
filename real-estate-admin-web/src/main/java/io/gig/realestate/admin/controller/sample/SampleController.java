af4ba97910367e5a63ee07855355d4046203ed10
kjuiop <copying_y@naver.com>
2023-09-02 10:40:51 +0900

    chore: 샘플 페이지 컨틀로러에서 분리

---
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/sample/SampleController.java
@@ -0,0 +1,36 @@
+package io.gig.realestate.admin.controller.sample;
+
+import io.gig.realestate.domain.realestate.dto.RealEstateDetailDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author : JAKE
+ * @date : 2023/09/02
+ */
+@Controller
+@RequestMapping("sample")
+@RequiredArgsConstructor
+public class SampleController {
+
+    @GetMapping("realestate/sample")
+    public String sample(Model model) {
+
+        RealEstateDetailDto dto = RealEstateDetailDto.emptyDto();
+        model.addAttribute("dto", dto);
+
+        return "sample/realestate/sample";
+    }
+
+    @GetMapping("realestate/sample2")
+    public String sample2(Model model) {
+
+        RealEstateDetailDto dto = RealEstateDetailDto.emptyDto();
+        model.addAttribute("dto", dto);
+
+        return "sample/realestate/sample2";
+    }
+}
