25e2a6e352276d1d91cccf94955875fbc9674cf7
kjuiop <copying_y@naver.com>
2023-09-14 01:02:36 +0900

    [#40] feat: validation modal oneBtnModal adD

--- real-estate-admin-web/src/main/resources/static/js/login/login.js
+++ real-estate-admin-web/src/main/resources/static/js/login/login.js
@@ -8,12 +8,31 @@ let signUpModal = function(e) {
     $('#sign-up-modal').modal('show');
 }
 
-const initICheck = function() {
+let initICheck = function() {
     $('input[type="checkbox"], input[type="radio"]').iCheck({
         checkboxClass: 'icheckbox_square-blue',
         radioClass: 'iradio_flat-blue'
     });
 };
 
+let signUp = function(e) {
+    e.preventDefault();
+
+    let isPrivacyAgree = $('#privacyAgree').prop('checked');
+    if (!isPrivacyAgree) {
+        oneBtnModal('개인 정보 수집 및 이용에 동의해주세요.');
+        return;
+    }
+
+    let isPolicyAgree = $('#policyAgree').prop('checked');
+    if (!isPolicyAgree) {
+        oneBtnModal('이용약관에 동의해주세요.');
+        return;
+    }
+
+    console.log("ee")
+}
+
 $(document).ready(onReady)
     .on('click', '#btnSignUpModal', signUpModal)
+    .on('click', '#btnSignUpSubmit', signUp);
