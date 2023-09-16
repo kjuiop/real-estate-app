dc584efc1b57586f0f9d7dd2f2ba8be1a4fdaf14
kjuiop <copying_y@naver.com>
2023-09-16 17:06:20 +0900

    [#46] feat: 계정 비밀번호 틀렸을 때 로그인 block

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/config/security/AuthenticationFailureHandlerImpl.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/config/security/AuthenticationFailureHandlerImpl.java
@@ -2,6 +2,7 @@ package io.gig.realestate.admin.config.security;
 
 import io.gig.realestate.domain.admin.AdministratorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.LockedException;
@@ -20,6 +21,7 @@ import java.net.URLEncoder;
  * @author : JAKE
  * @date : 2023/02/28
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthenticationFailureHandlerImpl implements AuthenticationFailureHandler {
@@ -28,9 +30,10 @@ public class AuthenticationFailureHandlerImpl implements AuthenticationFailureHa
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String username = request.getParameter("username");
         String errorMessage = null;
         if (exception instanceof BadCredentialsException || exception instanceof InternalAuthenticationServiceException) {
-            errorMessage = "계정 정보가 잘못되었습니다.";
+            errorMessage = "계정 정보가 올바르지 않습니다.";
             administratorService.increasePasswordFailureCount(request.getParameter("username"));
         } else  if (exception instanceof LockedException) {
             errorMessage = "패스워드 5회 이상 틀려 계정이 잠겼습니다.";
@@ -38,10 +41,13 @@ public class AuthenticationFailureHandlerImpl implements AuthenticationFailureHa
             errorMessage = "알 수 없는 문제가 발생하였습니다.";
         }
 
+        request.setAttribute("exception", exception.getClass().getName().replace(exception.getClass().getPackageName() + ".", ""));
+        request.setAttribute("exceptionMessage", errorMessage);
 
-        HttpSession session = request.getSession();
-        session.setAttribute("exceptionMessage", errorMessage);
-        response.sendRedirect(request.getContextPath() + "/login");
+        log.error("Authentication fail username: {}, message: {}", username, exception);
+//        request.getRequestDispatcher("/login?error=500").forward(request, response);
+
+        response.sendRedirect(request.getContextPath() + "/login?error=500");
     }
 
 }
