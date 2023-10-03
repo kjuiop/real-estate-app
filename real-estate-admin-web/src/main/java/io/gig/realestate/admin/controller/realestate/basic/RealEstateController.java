3ee532350777a52d5caf3da7490eb9d1bdb71a4b
kjuiop <copying_y@naver.com>
2023-10-03 19:34:12 +0900

    [#67] feat: 불필요한 데이터 조회 삭제

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/realestate/basic/RealEstateController.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/realestate/basic/RealEstateController.java
@@ -79,10 +79,7 @@ public class RealEstateController {
     public String editForm(@PathVariable(name = "realEstateId") Long realEstateId, Model model, @CurrentUser LoginUser loginUser) throws IOException {
 
         RealEstateDetailDto dto = realEstateService.getDetail(realEstateId);
-        List<LandDataApiDto> landList = landService.getLandListInfo(dto.getLegalCode(), dto.getLandType(), dto.getBun(), dto.getJi());
-        ConstructDataApiDto constructInfo = constructService.getConstructInfo(dto.getLegalCode(), dto.getLandType(), dto.getBun(), dto.getJi());
         List<ConstructFloorDataApiDto> floorInfo = constructService.getConstructFloorInfo(dto.getLegalCode(), dto.getLandType(), dto.getBun(), dto.getJi());
-
         List<AdministratorListDto> admins = administratorService.getAdminListMyMembers(loginUser);
         List<CategoryDto> processCds = categoryService.getChildrenCategoryDtosByName("진행구분");
         CategoryDto usageCds = categoryService.getCategoryDtoWithChildrenByName("매물용도");
@@ -91,11 +88,7 @@ public class RealEstateController {
         model.addAttribute("admins", admins);
         model.addAttribute("processCds", processCds);
         model.addAttribute("usageCds", usageCds);
-
-        model.addAttribute("landInfo", landList.get(0));
-        model.addAttribute("constructInfo", constructInfo);
         model.addAttribute("floorInfo", floorInfo);
-        model.addAttribute("landList", landList);
 
         return "realestate/editor";
     }
