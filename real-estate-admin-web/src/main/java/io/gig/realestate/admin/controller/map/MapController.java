85d6982ad46c652ecf411b44e2c74d4937f4007d
kjuiop <copying_y@naver.com>
2023-11-22 18:36:15 +0900

    [#102] feat: 지도검색화면에 매물 표시

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/map/MapController.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/map/MapController.java
@@ -3,14 +3,19 @@ package io.gig.realestate.admin.controller.map;
 import io.gig.realestate.domain.area.AreaService;
 import io.gig.realestate.domain.area.dto.AreaListDto;
 import io.gig.realestate.domain.realestate.basic.RealEstateSearchDto;
+import io.gig.realestate.domain.realestate.basic.RealEstateService;
 import io.gig.realestate.domain.realestate.basic.dto.RealEstateDetailDto;
+import io.gig.realestate.domain.realestate.basic.dto.RealEstateListDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -22,10 +27,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MapController {
 
+    private final RealEstateService realEstateService;
     private final AreaService areaService;
 
     @GetMapping
-    public String map(RealEstateSearchDto searchDto, Model model) {
+    public String map(HttpServletRequest request, RealEstateSearchDto searchDto, Model model) {
+
+        HttpSession session = request.getSession();
 
         List<AreaListDto> sidoList = areaService.getParentAreaList();
         model.addAttribute("sidoList", sidoList);
@@ -40,7 +48,10 @@ public class MapController {
             model.addAttribute("dongList", dongList);
         }
 
+        Page<RealEstateListDto> pages = realEstateService.getRealEstatePageListBySearch(session.getId(), searchDto);
+
         model.addAttribute("condition", searchDto);
+        model.addAttribute("pages", pages);
 
         return "map/map";
     }
