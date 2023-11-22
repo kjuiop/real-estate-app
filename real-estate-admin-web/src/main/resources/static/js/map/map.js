23b43f7fb7d95116651f03712d36eeb62e15ab85
kjuiop <copying_y@naver.com>
2023-11-22 11:10:10 +0900

    [#102] feat: 지도 화면 구성

--- real-estate-admin-web/src/main/resources/static/js/map/map.js
+++ real-estate-admin-web/src/main/resources/static/js/map/map.js
@@ -2,4 +2,9 @@ let onReady = function() {
     loadKakaoMap('서울특별시 강남구 도산대로 149');
 }
 
-$(document).ready(onReady);
+let cursorItem = function(e) {
+    alert("in");
+
+}
+$(document).ready(onReady)
+    .on('cursor', '.real-estate-unit', cursorItem);
