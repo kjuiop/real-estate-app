581f22750a34a371d11ea565b0a1bbd6e4b93981
kjuiop <copying_y@naver.com>
2023-11-28 22:10:53 +0900

    [#111] feat: 임대면적 자동 채우기 기능 추가

--- real-estate-domain/src/main/java/io/gig/realestate/domain/realestate/construct/dto/ConstructFloorDataApiDto.java
+++ real-estate-domain/src/main/java/io/gig/realestate/domain/realestate/construct/dto/ConstructFloorDataApiDto.java
@@ -28,14 +28,30 @@ public class ConstructFloorDataApiDto {
     // 부용도
     private String etcPurps;
 
+    private double lndpclAr;
+
+    private double lndpclArByPyung;
+
     public static ConstructFloorDataApiDto convertData(JSONObject item) {
         return ConstructFloorDataApiDto.builder()
                 .flrNo(item.has("flrNo") ? item.optInt("flrNo") : 0)
                 .flrNoNm(item.has("flrNoNm") ? item.optString("flrNoNm") : null)
                 .area(item.has("area") ? item.optDouble("area") : null)
+                .lndpclAr(item.has("area") ? item.optDouble("area") : 0.0)
+                .lndpclArByPyung(item.has("area") ? calculatePyung(item.optDouble("area")) : 0.0)
                 .mainPurpsCdNm(item.has("mainPurpsCdNm") ? item.optString("mainPurpsCdNm") : null)
                 .etcPurps(item.has("etcPurps") ? item.optString("etcPurps") : null)
                 .build();
+    }
+
+    private static double calculatePyung(Double area) {
+        if (area == null || area == 0) {
+            return 0.0;
+        }
+
+        double result = area / 3.305785;
+        result = Math.round(result * 100.0) / 100.0;
+        return result;
     }
 
 
