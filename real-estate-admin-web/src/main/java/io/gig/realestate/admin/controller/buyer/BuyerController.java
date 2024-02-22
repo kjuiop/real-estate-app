ac22d4c89ab1840c84dc8abb163554f091e15d3d
kjuiop <copying_y@naver.com>
2024-02-22 11:09:15 +0900

    코드기준으로 자식 코드 조회 기능 추가

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/buyer/BuyerController.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/buyer/BuyerController.java
@@ -4,6 +4,8 @@ import io.gig.realestate.domain.area.AreaService;
 import io.gig.realestate.domain.area.dto.AreaListDto;
 import io.gig.realestate.domain.buyer.dto.BuyerDetailDto;
 import io.gig.realestate.domain.buyer.dto.BuyerSearchDto;
+import io.gig.realestate.domain.category.CategoryService;
+import io.gig.realestate.domain.category.dto.CategoryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,11 +24,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BuyerController {
 
+    private final CategoryService categoryService;
     private final AreaService areaService;
 
     @GetMapping
     public String index(BuyerSearchDto condition, Model model) {
         model.addAttribute("condition", condition);
+        model.addAttribute("processCds", categoryService.getChildrenCategoryDtosByCode("CD_PROCESS"));
         return "buyer/list";
     }
 
