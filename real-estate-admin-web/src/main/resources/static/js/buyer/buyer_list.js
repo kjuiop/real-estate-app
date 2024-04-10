71ae567adfc237f089e22a2b32d79c6a8028f36a
kjuiop <copying_y@naver.com>
2024-04-10 12:23:59 +0900

    [#21] feat: 선택된 매물 정보 연동 기능 구현

--- real-estate-admin-web/src/main/resources/static/js/buyer/buyer_list.js
+++ real-estate-admin-web/src/main/resources/static/js/buyer/buyer_list.js
@@ -370,12 +370,62 @@ let drawRealEstateTable = function(list) {
         tag += '<td class="text-alien-center">';
         tag += '<input type="checkbox" class="checkElement" name="realEstateId" value="' + item.realEstateId + '">';
         tag += '</td>';
-        tag += '<td>' + item.salePrice + '억</td>';
-        tag += '<td>' + item.address + '</td>';
-        tag += '<td>' + item.managerName + '</td>';
-        tag += '<td>' + moment(item.createdAt).format("YYYY-MM-DD") + '</td>';
+        tag += '<td class="salePrice" salePrice="' + item.salePrice + '">' + item.salePrice + '억</td>';
+        tag += '<td class="address" address="' + item.address + '">' + item.address + '</td>';
+        tag += '<td class="lndpclArByPyung">' + item.lndpclArByPyung.toFixed(2) + '평';
+        tag += '<input type="hidden" name="lndpclArByPyung" value="' + item.lndpclArByPyung.toFixed(2) + '" />';
+        tag += '</td>';
+        tag += '<td class="totAreaByPyung">' + item.totAreaByPyung.toFixed(2) + '평';
+        tag += '<input type="hidden" name="totAreaByPyung" value="' + item.totAreaByPyung.toFixed(2) + '" />';
+        tag += '</td>';
+        tag += '<td class="archAreaByPyung">' + item.archAreaByPyung.toFixed(2) + '평';
+        tag += '<input type="hidden" name="archAreaByPyung" value="' + item.archAreaByPyung.toFixed(2) + '" />';
+        tag += '</td>';
+        tag += '<td class="managerName" managerName="' + item.managerName + '">' + item.managerName + '</td>';
+        // tag += '<td class="createdAt" createdAt="' + moment(item.createdAt).format("YYYY-MM-DD") + '">' + moment(item.createdAt).format("YYYY-MM-DD") + '</td>';
         tag += '</tr>';
     });
+    return tag;
+}
+
+let applyRealEstate = function(e) {
+    e.preventDefault();
+
+    let $modal = $('#searchRealEstateModal'),
+        $tbody = $modal.find('.searchTable tbody');
+
+    let tag = '';
+    $tbody.find('tr').each(function(idx, item){
+        let isChecked = $(item).find('input[name="realEstateId"]').prop('checked');
+        if (isChecked) {
+            let realEstate = {
+                "realEstateId": $(item).find('input[name="realEstateId"]').val(),
+                "salePrice": $(item).find('.salePrice').attr('salePrice'),
+                "address": $(item).find('.address').attr('address'),
+                "managerName": $(item).find('.managerName').attr('managerName'),
+                "lndpclArByPyung": $(item).find('input[name="lndpclArByPyung"]').val(),
+                "totAreaByPyung": $(item).find('input[name="totAreaByPyung"]').val(),
+                "archAreaByPyung": $(item).find('input[name="archAreaByPyung"]').val(),
+                "createdAt": $(item).find('.createdAt').attr('createdAt')
+            }
+            tag += drawSelectedItem(realEstate);
+        }
+    });
+    $('#historyModal').find('.realEstateTable tbody').html(tag);
+    $modal.find('.btnClose').trigger('click');
+}
+
+let drawSelectedItem = function(realEstate) {
+    let tag = '';
+    tag += '<tr>';
+    tag += '<td class="text-alien-center" style="width:30%;">' + realEstate.address + '</td>';
+    tag += '<td class="text-alien-center" style="width:15%;">' + realEstate.salePrice + '억원</td>';
+    tag += '<td class="text-alien-center" style="width:10%;">' + realEstate.lndpclArByPyung + '평</td>';
+    tag += '<td class="text-alien-center" style="width:10%;">' + realEstate.lndpclArByPyung + '평</td>';
+    tag += '<td class="text-alien-center" style="width:10%;">' + realEstate.lndpclArByPyung + '평</td>';
+    tag += '<td class="text-alien-center" style="width:15%;">' + realEstate.managerName + '</td>';
+    tag += '<td class="text-alien-center" style="width:10%;"><button type="button" class="btn btn-xs btn-danger btnRemove">삭제</button></td>';
+    tag += '</tr>';
     return tag;
 }
 
@@ -391,4 +441,5 @@ $(document).ready(onReady)
     .on('click', '.btnHistoryMapModal', showHistoryMapModal)
     .on('click', '.btnHistoryMapAdd', addHistoryMap)
     .on('click', '.btnSearch', searchRealEstate)
+    .on('click', '.btnApply', applyRealEstate)
 ;
