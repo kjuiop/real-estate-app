66492e0757fb0923e75b9276aadb28d57b54feed
kjuiop <copying_y@naver.com>
2024-04-29 18:14:23 +0900

    feat: global exception handler

---
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/error/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package io.gig.realestate.admin.controller.error;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * @author : JAKE
+ * @date : 2024/04/29
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
