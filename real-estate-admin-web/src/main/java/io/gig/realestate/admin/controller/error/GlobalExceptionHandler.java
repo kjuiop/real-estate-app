810768350ac6e0b192395b2e65f61b524145ef8c
kjuiop <copying_y@naver.com>
2024-05-01 13:20:26 +0900

    [#29] feat : error slack report

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/error/GlobalExceptionHandler.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/error/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package io.gig.realestate.admin.controller.error;
 
+import io.gig.realestate.domain.message.slack.SlackService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,13 +12,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  * @date : 2024/04/29
  */
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final SlackService slackService;
 
     @ExceptionHandler(value = Exception.class)
     public String handle(Exception e) {
-        log.error("hello " + e.getMessage());
+        slackService.sendMessage(e.getMessage());
+        log.error("An error occurred: {}", e.getMessage(), e);
         return "error/500";
     }
 }
---
+++ real-estate-domain/src/main/java/io/gig/realestate/domain/utils/properties/SlackProperties.java
@@ -0,0 +1,21 @@
+package io.gig.realestate.domain.utils.properties;
+
+import io.gig.realestate.domain.utils.YamlPropertySourceFactory;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : JAKE
+ * @date : 2024/05/01
+ */
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "slack")
+@PropertySource(value="classpath:/application-credentials.yml", factory = YamlPropertySourceFactory.class)
+public class SlackProperties {
+    String url;
+}
