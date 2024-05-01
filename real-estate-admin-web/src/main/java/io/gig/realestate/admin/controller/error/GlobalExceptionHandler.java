328b1f971b0ece24b12feaf5a08c9ccf2afbe1a3
kjuiop <copying_y@naver.com>
2024-05-01 11:42:23 +0900

    [#29] feat: exception handler add

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/error/GlobalExceptionHandler.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/error/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package io.gig.realestate.admin.controller.error;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * @author : JAKE
@@ -8,4 +11,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(value = Exception.class)
+    public String handle(Exception e) {
+        log.error("hello " + e.getMessage());
+        return "error/500";
+    }
 }
