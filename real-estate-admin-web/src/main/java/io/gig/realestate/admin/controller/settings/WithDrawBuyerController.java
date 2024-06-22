339738e8b6323dad1a299410553418e18c927e92
kjuiop <copying_y@naver.com>
2024-06-22 18:28:28 +0900

    [#53] feat: 담당자 이관 select box 생성

--- real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/settings/WithDrawBuyerController.java
+++ real-estate-admin-web/src/main/java/io/gig/realestate/admin/controller/settings/WithDrawBuyerController.java
@@ -1,7 +1,9 @@
 package io.gig.realestate.admin.controller.settings;
 
+import io.gig.realestate.domain.admin.AdministratorService;
 import io.gig.realestate.domain.admin.LoginUser;
 import io.gig.realestate.domain.buyer.basic.BuyerService;
+import io.gig.realestate.domain.buyer.basic.dto.BuyerDetailDto;
 import io.gig.realestate.domain.buyer.basic.dto.BuyerListDto;
 import io.gig.realestate.domain.buyer.basic.dto.BuyerSearchDto;
 import io.gig.realestate.domain.category.CategoryService;
@@ -12,7 +14,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author : JAKE
@@ -26,6 +32,7 @@ public class WithDrawBuyerController {
     private final BuyerService buyerService;
     private final CategoryService categoryService;
     private final TeamService teamService;
+    private final AdministratorService administratorService;
 
     @GetMapping
     public String index(BuyerSearchDto condition, @CurrentUser LoginUser loginUser, Model model) {
@@ -38,8 +45,29 @@ public class WithDrawBuyerController {
         model.addAttribute("purposeCds", categoryService.getChildrenCategoryDtosByCode("CD_PURPOSE"));
         model.addAttribute("processCds", categoryService.getChildrenCategoryDtosByCode("CD_PROCESS"));
         model.addAttribute("teams", teamService.getTeamList());
+        model.addAttribute("admins", administratorService.getAdminListMyMembers(loginUser));
         return "settings/withdraw/buyer/list";
     }
 
+    @GetMapping("{buyerId}/edit")
+    public String editForm(@PathVariable(name = "buyerId") Long buyerId,
+                           @CurrentUser LoginUser loginUser,
+                           HttpServletRequest request,
+                           Model model) throws AuthenticationException {
+        BuyerDetailDto dto = buyerService.getBuyerDetail(buyerId);
+
+        model.addAttribute("dto", dto);
+        model.addAttribute("loginUser", loginUser);
+        model.addAttribute("buyerGradeCds", categoryService.getChildrenCategoryDtosByCode("CD_BUYER_GRADE"));
+        model.addAttribute("characterCds", categoryService.getChildrenCategoryDtosByCode("CD_INVESTMENT_CHARACTER"));
+        model.addAttribute("purposeCds", categoryService.getChildrenCategoryDtosByCode("CD_PURPOSE"));
+        model.addAttribute("loanCharacterCds", categoryService.getChildrenCategoryDtosByCode("CD_LOAN_CHARACTER"));
+        model.addAttribute("preferBuildingCds", categoryService.getChildrenCategoryDtosByCode("CD_PREFER_BUILDING"));
+        model.addAttribute("investmentTimingCds", categoryService.getChildrenCategoryDtosByCode("CD_INVESTMENT_TIMING"));
+        model.addAttribute("processCds", categoryService.getChildrenCategoryDtosByCode("CD_PROCESS"));
+        model.addAttribute("teams", teamService.getTeamList());
+        model.addAttribute("admins", administratorService.getTeamAdminListByLoginUser(loginUser));
+        return "settings/withdraw/buyer/editor";
+    }
 
 }
